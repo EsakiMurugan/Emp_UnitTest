37f4aeb40127018b257fe2d5402628d584c7192c
ESAKI-1 <ESAKI-1@GF1KDYYL9P6P>
2022-07-14 12:45:03 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_Ut
+{
+    public class Employee : IEmployee<Employee>
+    {
+        public int Eid { get; set; }
+        public string EName { get; set; }
+        public float Salary { get; set; }
+        public Employee() { }
+
+        public Employee(int eid, string eName, float salary)
+        {
+            Eid = eid;
+            EName = eName;
+            Salary = salary;
+        }
+        public static List<Employee> employees = new List<Employee>();
+
+        public bool AddEmployee(Employee e)
+        {
+            employees.Add(e);
+            return true;
+        }
+
+        public bool checksal(int sal)
+        {
+            int result = add(sal, 20);
+            if (result > 50)
+                return true;
+            else
+                return false;   
+        }
+        public int add(int x,int y)
+        {
+            return x + y;
+        }
+
+        public Employee GetEmpById(int id)
+        {
+            Employee e = employees.Where(x => x.Eid == id).SingleOrDefault();
+            return e;
+        }
+
+        public List<Employee> GetEmpList()
+        {
+            employees.Add(new Employee(1, "Esaki", 25000));
+            employees.Add(new Employee(2,"Faisal", 30000));
+            return employees;
+
+        }
+
+        public string message(string name)
+        {
+            return "Hello" + name;
+        }
+
+        public bool RemoveEmployee(int id)
+        {
+            Employee e = GetEmpById(id);
+            employees.Remove(e);
+            return true;
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Emp_Ut
+{
+    public class Program
+    {
+        private static List<Employee> emps = new List<Employee>();
+
+        static void Main(string[] args)
+        {
+            IEmployee<Employee> obj = new Employee();
+            int result = obj.add(5, 4);
+            Console.WriteLine(result);
+            emps = obj.GetEmpList();
+            Employee e1 = obj.GetEmpById(1);
+            Console.WriteLine(e1.EName = " " + e1.Salary);
+        }
+    }
+}
